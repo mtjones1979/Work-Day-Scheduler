4b2db674f6822b07dde4d0db133348e311d8da80
Michael Jones <mtjones1979@yahoo.com>
2021-01-25 17:49:01 -0600

    finally got color coding to work

---
+++ oldscript.js
@@ -0,0 +1,97 @@
+// review Unit 5 before
+// commented out my previous try at the scheduler lay out - could not get it 
+// to work and found a different way but wanted to make sure you saw my progress
+
+
+$(document).ready(function() {
+   
+    // need date and current time to show up and connect
+    // to #currentDay - look up create element in jquery
+    // function init(){
+    var presentTime = moment().format('MMMM Do YYYY, h:mm a');
+    let $currentDay = $("#currentDay");
+    $currentDay.text(presentTime);
+    console.log(currentDay);
+    // }
+    var $calendar = $(".container");
+    $calendar.empty();
+    var timeBlocks = [];
+    var entries = [];
+    // build rows and columns in jquery not javascript - 
+   // need enough rows for set time of working hours
+   
+       for (let i = 9; i < 18; i++) {
+    
+    // 1st section on event Planner that shows time
+    // row should start with time (hourly - 1/4 row)
+    // set time frame on calendar also .time-block
+
+    // I spent lots of time writing out element creation like last homework and it did not look right
+    // found on jquery about adding all together - works and looks better
+       var $rowDiv = $('<div data-time=${i} id="${i}" class="row">');
+        // $rowDiv.addClass("row");
+        // $rowDiv.attr(");
+        // $rowDiv.attr("hour-index", i);
+       
+       var $timeDiv = $('<div class="col-md-2"><p class="hour">' + getAMPM(i) + '</p>');
+        // $timeDiv.addClass('col-md-2');
+        // $timeDiv.attr("class", "time-block");
+        // var $timeBlock = $("<span>")
+        // $timeBlock.attr("class", "time-block");
+        // $timeBlock.text("${displayHour} ${ampm}");
+        // $rowDiv.append($timeDiv);
+        // $timeDiv.append($timeBlock);
+  
+        
+        
+        // 2nd part of planner that shows text input
+        // text area to put event (span/label?/look back at hw 4)
+        // (textarea in CSS)
+        var $eventPlanner = $('<div class="col-md-9 past"><textarea id=text${i} class="description"></textarea>');
+
+        // $eventPlanner.attr("class", "input-{index}");
+        // $eventPlanner.attr("hour-index", index);
+        // $eventPlanner.attr("type", "text");
+        // $eventPlanner.attr("id", "textarea");
+        // $eventPlanner.val(planText[index]);
+        // console.log($eventPlanner);
+        // var $inputDiv = $("<div>");
+        // $inputDiv.addClass("col-md-9");
+        // $inputDiv.attr("id", "textarea")
+        // $rowDiv.append($inputDiv);
+        // $inputDiv.append($eventPlanner);
+        
+        var $buttonDiv = $('<div class="col-md-1"><button class="saveBtn" id=${i}><i class="fas fa-save"></i>');
+        // var $buttonDiv = $('<div>');
+        // $buttonDiv.addClass("col-md-1");
+        // $buttonDiv.addClass("saveBtn", i);
+        // var $saveBtn = $("<i>");
+        // $saveBtn.attr("class", "${index}");
+        // $saveBtn.attr("saveBtn", i);
+        // $saveBtn.attr("class", "fas fa-save saveBtn");
+        // $rowDiv.append($buttonDiv);
+        // $buttonDiv.append($saveBtn);
+        // $updateRowColor($rowDiv, hour);
+        // $calendar.append($rowDiv);
+  
+            $rowDiv.append($timeDiv);
+            $rowDiv.append($eventPlanner);
+            $rowDiv.append($buttonDiv);
+
+            $calendar.append($rowDiv);
+            timeBlocks.push($rowDiv);
+            // getLocalStorage(i); 
+    }
+        // console.log(getLocalStorage);
+    function getAMPM(hours){
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        return hours + ampm;
+    }
+    getAMPM();
+    // create function to link current time to time on planner
+    // past hours need to (.past)
+    // present hour need to (.present) 
+    // future hours need to (.future)
+});
--- script.js
+++ script.js
@@ -1,120 +1,115 @@
 // review Unit 5 before
-// create function to input text and tie to internal storage
-function getLocalStorage(key){
-    let value = localStorage.getItem(key);
-    if(value){
-        $('#text${i}').text(value);
-    }
-}
-
-$(document).ready(function() {
-   
-    // need date and current time to show up and connect
+$(document).ready(function() {   
+// need date and current time to show up and connect
     // to #currentDay - look up create element in jquery
     var presentTime = moment().format('MMMM Do YYYY, h:mm a');
     let $currentDay = $("#currentDay");
     $currentDay.text(presentTime);
     console.log(currentDay);
 
-    var $calendar = $(".container");
-    $calendar.empty();
 
+    var $calendar = $(".container");
     // build rows and columns in jquery not javascript - 
    // need enough rows for set time of working hours
-   
+
        for (let i = 9; i < 18; i++) {
-            // let index = hour - 9;
+    
     // 1st section on event Planner that shows time
     // row should start with time (hourly - 1/4 row)
     // set time frame on calendar also .time-block
 
-    // I spent lots of time writing out elements creation like last homework
-    // found on jquery about adding all together - works better 
-       var $rowDiv = $('<div data-time=${i} id="${i}" class="row">');
-    //    $rowDiv.addClass("row");
-    //    $rowDiv.attr(");
-    //    $rowDiv.attr("hour-index", i);
+    // I spent lots of time writing out element creation like last homework and it did not look right nor work
+    // found on jquery about adding all together - works and looks better
+       var $rowDiv = $(`<div data-time=${i} id="${i}" class="row">`);
+    
        
-       var $timeDiv = $('<div class="col-md-2"><p class="hour">' + formatAMPM(i) + '</p>');
-    //    $timeDiv.addClass('col-md-2');
-    //    $timeDiv.attr("class", "time-block");
-
-    //    var $timeBlock = $("<span>")
-    //    $timeBlock.attr("class", "time-block");
-
-    //    let displayHour = 0;
-    //    let ampm = "";
-    //    if (i > 12) {
-    //        displayHour = i - 12;
-    //        ampm = "pm";
-    //     } else {
-    //        displayHour = i;
-    //         ampm = "am";
-            
-        // }
-        // $timeBlock.text("${displayHour} ${ampm}");
-
-        // $rowDiv.append($timeDiv);
-        // $timeDiv.append($timeBlock);
-  
-        
-        
+       var $timeDiv = $('<div class="col-md-2"><p class="hour">' + getAMPM(i) + '</p>');
         // 2nd part of planner that shows text input
         // text area to put event (span/label?/look back at hw 4)
         // (textarea in CSS)
-        var $eventPlanner = $('<div class="col-md-8 past"><textarea id=text${i} class="description"></textarea>');
-
-        // $eventPlanner.attr("class", "input-{index}");
-        // // $eventPlanner.attr("hour-index", index);
-        // $eventPlanner.attr("type", "text");
-        // // $eventPlanner.attr("id", "textarea");
-
-        // // $eventPlanner.val(planText[index]);
-        // console.log($eventPlanner);
-        // var $inputDiv = $("<div>");
-        // $inputDiv.addClass("col-md-9");
-        // $inputDiv.attr("id", "textarea")
+        var $eventPlanner = $(`<div class="col-md-8"><textarea id=text${i} class="description"></textarea>`);
         
-        // $rowDiv.append($inputDiv);
-        // $inputDiv.append($eventPlanner);
-        
-
-    // button (.saveBtn, .saveBtn i: hover to click on to save content - 
-    
-        // var saveBtn = ""
-        var $buttonDiv = $('<div class="col-md-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i>');
-        // var $buttonDiv = $('<div>');
-        // $buttonDiv.addClass("col-md-1");
-        // $buttonDiv.addClass("saveBtn", i);
-        // var $saveBtn = $("<i>");
-        // // $saveBtn.attr("class", "${index}");
-        // // $saveBtn.attr("saveBtn", i);
-        // $saveBtn.attr("class", "fas fa-save saveBtn text-center");
-
-        // $rowDiv.append($buttonDiv);
-        // $buttonDiv.append($saveBtn);
-
-    //     // $updateRowColor($rowDiv, hour);
-    //     $calendar.append($rowDiv);
-    // console.log($rowDiv);
+        var $buttonDiv = $(`<div class="col-md-2"><button class="saveBtn" id=${i}><i class="fas fa-save saveBtn"></i></button>`);
+            
             $rowDiv.append($timeDiv);
             $rowDiv.append($eventPlanner);
             $rowDiv.append($buttonDiv);
 
-            $(".container").append($rowDiv);
-            getLocalStorage(i);
-    }
-
-    function formatAMPM(hours){
+            $calendar.append($rowDiv);
+            // localStorage(i);
+        }
+            // var timeBlock = $eventPlanner;
+            // var time = parseInt(moment().format("HH"));
+            //   $.each(timeBlock, function (i, hours){
+            //       var selectedText = parseInt($(this).attr("id"));
+            //     //   var time = moment().format("HH");
+            //       if (selectedText > time) {
+            //           $(this).addClass("future")
+            //       } else if (selectedText === time){
+            //           $(this).addClass("present")
+            //       } else {
+            //           $(this).addClass("past");
+            //       };
+            //     //   $("#" + selectedText).val(localStorage.getItem(selectedText))
+            //   });
+            // console.log(timeBlock)
+     
+    
+        
+      
+        // function to show time in 1st row
+    function getAMPM(hours){
         var ampm = hours >= 12 ? 'pm' : 'am';
         hours = hours % 12;
         hours = hours ? hours : 12;
         return hours + ampm;
     }
-    formatAMPM();
+    getAMPM();
     // create function to link current time to time on planner
     // past hours need to (.past)
     // present hour need to (.present) 
     // future hours need to (.future)
+        // function setBlockColor(){
+        //   timeBlocks.forEach(function (block, i){
+        //     currentId = block.attr("data-hour");
+        //     var timeBlocks = block.find(".time-block");
+
+        //     if (currentId < presentTime) {
+        //         timeBlocks.addClass("past");
+        //     } else if (currentId > presentTime){
+        //         timeBlocks.addClass("future");
+        //     } else if (currentId === presentTime){
+        //         timeBlocks.addClass("present");
+        //     }
+        //   });
+        // if i 
+        function colorCoding(){
+            var currentTime = new Date().getHours();
+            for (var i = 9; i < 18; i++) { 
+            
+             if ($(`#${i}`).data("time") == currentTime){
+                $(`#text${i}`).addClass( "present");
+            } else if (currentTime < $(`#${i}`).data("time")) {
+                $(`#text${i}`).addClass( "future");
+            } else $(`#text${i}`).addClass("past");
+        }
+    }
+    console.log(colorCoding);
+    setInterval(function() {
+        colorCoding();
+    }, 1000);
+        
+    var saveBtn = $('.saveBtn');
+saveBtn.on('click', function(){
+    let eventId = $(this).attr('id');
+    let eventText = $(this).parent().siblings().children('.description').val();
+    localStorage.setItem(eventId, eventText);
+    });
 
 });
+       
+       console.log(saveBtn)
+    
+ // create function to input text and tie to internal storage
+    console.log(this);
+
